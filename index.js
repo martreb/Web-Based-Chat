570f12d6b1bb8ce85fe10844abed4c36d3714c72
EganJ <EganJ@users.noreply.github.com>
2019-10-30 15:35:31 -0500

    Server to client

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 //In Javascript, require is similar to import.
 //Express is the library we will be using. It's documentation is availiable at https://expressjs.com/ 
 const express = require("express");
+const bodyParser = require("body-parser");
 const port = process.env.port || 3000;
 //calling express creates a server. Right now the server is not listening to any port/url.
 var app = express();
 
+//parse the body
+app.use(bodyParser.raw());
+
 //here we set the app to serve static files from the folder public.
 app.use(express.static("public"));
+
+app.get("/chat", (request, response) => {
+    console.log("Recieving Get Request");
+    console.log(request.body);
+    response.send("Hello World");
+})
 
 app.listen(port);
--- public/clientchat.js
+++ public/clientchat.js
@@ -1,1 +1,24 @@
-alert("hello");
+//Javascript files run top-to-bottom at the time of their load.
+//initialize user's name
+var userName = prompt("Name:");
+//userName evaluates to false while equal to 0, "", false, null, or undefined. In any of these cases, ask for a different name.
+while (!userName) {
+    userName = prompt("Invalid Name. \nName:");
+}
+
+//the $ represents Jquery, the library we are using.
+//Post to server root, saying "we exist" and asking.
+/*$.get parameters:
+    -url, the url to make the request to. If no protocol (i.e. "http://"),
+        sends relative to host (i.i "localhost:3000/")
+    -data, data sent to the server.
+*/
+var data = { "query": "new user", "name": name }
+$.get("/chat", JSON.stringify(data)).done((data) => {
+    //handle success
+    alert(data);
+}).fail((data) => {
+    //handle error
+    alert(data);
+});
+console.log({ "query": "new user", "name": name });

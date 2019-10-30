17fb62bd8ee657606b6a12b05f216af66483445e
EganJ <EganJ@users.noreply.github.com>
2019-10-30 14:42:43 -0500

    Static Serving

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 //In Javascript, require is similar to import.
 //Express is the library we will be using. It's documentation is availiable at https://expressjs.com/ 
-express = require("express");
+const express = require("express");
+const port = process.env.port || 3000;
+//calling express creates a server. Right now the server is not listening to any port/url.
+var app = express();
+
+//here we set the app to serve static files from the folder public.
+app.use(express.static("public"));
+
+app.listen(port);

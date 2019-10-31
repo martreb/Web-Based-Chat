95ab7f41b2a39ab293eef876591f39f803e41ce5
EganJ <eg.co.johnson@gmail.com>
2019-10-31 15:42:31 +0000

    Inputs and message creation

--- public/clientchat.js
+++ public/clientchat.js
@@ -20,3 +20,42 @@ $.post("/chat", { "query": "new user", "name": name }).done((data) => {
     //handle error
     alert(data);
 });
+
+/**
+Handles logic for when the user enters a message
+@param {String} msg- the text entered by this page's user
+ */
+function messageEntered(msg) {
+    //Put server communication code here
+    addMessage(msg);
+}
+
+/**ß
+Creates the HTML elements to display a message
+@param {String} msg- message text
+ */
+function addMessage(msg) {
+    var messageBox = document.getElementById("messages");
+    //create and format div wrapper
+    var wrapper = document.createElement("div");
+    wrapper.className = "message";
+    //create paragraph text element
+    var text = document.createElement("p");
+    text.innerHTML = userName + ": " + msg;
+    //add children
+    wrapper.appendChild(text);
+    messageBox.appendChild(wrapper);
+}
+
+var textInput = document.getElementById("textInput");
+
+textInput.addEventListener("input", () => {
+    var inputString = textInput.value;
+    //check for newline. This expression looks really weird- take a look at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+    newlines = (/(\n|\r)/g).test(inputString);
+    if (newlines) {
+        messageEntered(inputString);
+        textInput.value = "";
+    }
+});
+
